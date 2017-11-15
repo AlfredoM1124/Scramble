4acee169f12b9a701333efa8f848f255ac22737f
dhurley94 <drhurley94@gmail.com>
2017-11-14 22:58:36 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Explore from './Components/Explore';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Explore />
+      </div>
+    );
+  }
+}
+
+export default App;
