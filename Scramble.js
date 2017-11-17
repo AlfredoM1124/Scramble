ec0fddc224e76452fa3064f6b8997f7e67fc1625
Alfredo Munoz <AlfredoM@Alfredos-MacBook-Pro.local>
2017-11-16 21:10:44 -0500

    Declared Dependencies and created instance of express app.

---
+++ Scramble.js
@@ -0,0 +1,9 @@
+// Declare Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Create an instance of the express app 
+var app = express();
+var routes = require("./controllers/scramble_controller.js");
+
+// 
