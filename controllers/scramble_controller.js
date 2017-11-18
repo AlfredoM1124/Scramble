962578ae7a5324101590433811926bda3ab33936
Alfredo Munoz <AlfredoM@Alfredos-MacBook-Pro.local>
2017-11-18 12:00:15 -0500

    Required express, and models.

---
+++ controllers/scramble_controller.js
@@ -0,0 +1,12 @@
+// Declare Dependencies
+var express = require("express");
+
+var router = express.Router();
+var app = express();
+
+// Import the Model to use its function
+var scramble = require("../models/users");
+
+// Create all our logic and routes
+
+
