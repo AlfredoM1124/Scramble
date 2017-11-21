0bfa1acaccc221362b5e4bcec104715055cef1ef
Alfredo Munoz <AlfredoM@Alfredos-MacBook-Pro.local>
2017-11-21 17:33:57 -0500

    Set up PORT and Listener

--- Scramble.js
+++ Scramble.js
@@ -6,4 +6,18 @@ var bodyParser = require("body-parser");
 var app = express();
 var routes = require("./controllers/scramble_controller.js");
 
-// 
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 3306;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  }); 
