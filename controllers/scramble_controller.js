25dd20ccfbb0d94967ae8cf6044ee7e75b97e01d
Alfredo Munoz <AlfredoM@Alfredos-MacBook-Pro.local>
2017-11-21 19:48:27 -0500

    Added some constructors for searching Users and creating Users

--- controllers/scramble_controller.js
+++ controllers/scramble_controller.js
@@ -12,6 +12,22 @@ var scramble = require("../models/users");
 
 
 // Functions for USER 
+var UserSearch = function(user, interests){
+    this.user = user;
+    this.interests = interests;
+    this.date = Date.now();
+
+};
+
+var createUser = function(name, gender, bio, interests){
+    this.name = name;
+    this.gender = gender;
+    this.bio = bio;
+    this.interests = interests;
+
+    
+};
+
 function createProfile (req, res){
 
 };
@@ -30,8 +46,11 @@ function profilePicture (req, res){
 function editBio (req, res){
 
 };
-
 // End USER Functions
+
+// Start ADMIN functions
+
+// End ADMIN functions
 
 // Export routes for scramble.js to use.
 module.exports = router;
