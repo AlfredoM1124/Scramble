7130d36b08e70acd60aa407832f602340dffe51d
Alfredo Munoz <AlfredoM@Alfredos-MacBook-Pro.local>
2017-11-21 19:28:50 -0500

    Added more function blueprints

--- controllers/scramble_controller.js
+++ controllers/scramble_controller.js
@@ -10,18 +10,28 @@ var scramble = require("../models/users");
 
 // Create all our logic and routes
 
+
+// Functions for USER 
 function createProfile (req, res){
 
 };
-
 function postReview (req, res){
 
 };
-
 function postComment (req, res){
 
 };
+function editInterests (req, res){
 
+};
+function profilePicture (req, res){
+
+};
+function editBio (req, res){
+
+};
+
+// End USER Functions
 
 // Export routes for scramble.js to use.
 module.exports = router;
