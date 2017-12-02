8be54aba721e394ed00954a6a79043210abdfed9
dhurley94 <drhurley94@gmail.com>
2017-12-01 23:10:27 -0500

    changed ports to 8484

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var pug = require('pug');
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8484;
 
 // Requiring our models for syncing
 var db = require("./models");
